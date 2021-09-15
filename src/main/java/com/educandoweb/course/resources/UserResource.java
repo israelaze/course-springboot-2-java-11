1df9b7a0ad9edc16c389eaf2479f21161ed73462
israelaze <israelaze@gmail.com>
2021-09-15 15:28:25 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L, "Israel", "is@gmail.com", "9855222", "1905");
+		return ResponseEntity.ok().body(user);
+	}
+	
+	
+	
+
+}
